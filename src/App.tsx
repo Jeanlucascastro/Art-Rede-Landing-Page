931aad0d9e74b89a9fc68df6d67a4d83e38e7e8e
Jean Castro <61846534+Jeanlucascastro@users.noreply.github.com>
2023-03-28 23:49:44 -0300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { icon } from '@fortawesome/fontawesome-svg-core';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+
+const whatsappIcon = icon(faWhatsapp);
+
+
+
+
 
 function App() {
+
+  function handleClick() {
+    window.location.href = 'https://api.whatsapp.com/message/2WYOQ3NUJ7NVP1?autoload=1&app_absent=0';
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div className="header">
+    <h1>Art Rede</h1>
+      </div>
+      <div className="App-header">
+    <button className="icon-container" onClick={handleClick}>
+    <FontAwesomeIcon icon={whatsappIcon}  className='whatsapp-icon'/>
+      <div className="icon">
+        <a href="https://api.whatsapp.com/message/2WYOQ3NUJ7NVP1?autoload=1&app_absent=0" className="whatsapp-link">
+          <h2>Clique para WhatsApp</h2>
         </a>
-      </header>
-    </div>
+      </div>
+    </button>
+  </div>
+</div>
+
   );
 }
 
