678f6a791708cfd5685a9be0b42480f19bb12753
Jean Castro <61846534+Jeanlucascastro@users.noreply.github.com>
2023-04-01 00:38:52 -0300

    Layout changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import gato from './969114_184494328389275_732875974_n.jpg';
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { icon } from '@fortawesome/fontawesome-svg-core';
@@ -23,15 +23,38 @@ function App() {
     <h1>Art Rede</h1>
       </div>
       <div className="App-header">
+        <div className='body'>
+        <p>Rede de Proteção para Apartamentos e Varais</p>
+        </div>
+        {/* <div className='imagem'>
+            <img src={gato} alt="Gato na rede de Proteção"></img>
+        </div> */}
+        <div className='redes'>
     <button className="icon-container" onClick={handleClick}>
     <FontAwesomeIcon icon={whatsappIcon}  className='whatsapp-icon'/>
       <div className="icon">
         <a href="https://api.whatsapp.com/message/2WYOQ3NUJ7NVP1?autoload=1&app_absent=0" className="whatsapp-link">
-          <h2>Clique para WhatsApp</h2>
+          <h2>WhatsApp</h2>
         </a>
       </div>
-      
     </button>
+    <button className="icon-container" onClick={handleClick}>
+    <FontAwesomeIcon icon={whatsappIcon}  className='whatsapp-icon'/>
+      <div className="icon">
+        <a href="https://api.whatsapp.com/message/2WYOQ3NUJ7NVP1?autoload=1&app_absent=0" className="whatsapp-link">
+          <h2>Instagram</h2>
+        </a>
+      </div>
+    </button>
+    <button className="icon-container" onClick={handleClick}>
+    <FontAwesomeIcon icon={whatsappIcon}  className='whatsapp-icon'/>
+      <div className="icon">
+        <a href="https://api.whatsapp.com/message/2WYOQ3NUJ7NVP1?autoload=1&app_absent=0" className="whatsapp-link">
+          <h2>Facebook</h2>
+        </a>
+      </div>
+    </button>
+    </div>
   </div>
 </div>
 
